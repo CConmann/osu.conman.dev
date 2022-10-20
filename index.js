4deed8eb968d3941d4d2a87954dc7a5ed22ccaf8
Conmann <dcdraper14@hotmail.com>
2022-10-19 20:36:43 -0500

    Added Settings Menu
    you can only change UI customization right now and it doesn't save

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-
+var customSettings = {},
+	scene;
+function SlogoFull(){
+	document.querySelectorAll('.SbuttonCont').forEach(function(el){
+		el.style.opacity = '0';
+	});
+	document.querySelector('.SflexCont').style.background = 'none'
+}
+function openSettings(){
+	document.querySelector('#settingsCont').style.display = 'flex';
+}
+function closeSettings(){
+	document.querySelector('#settingsCont').style.display = 'none';
+}
+function updateMenu(name){
+	if(name){
+		scene = name;
+	}
+}
 
 // var i = 0,
 // iaa = setInterval(function(){
