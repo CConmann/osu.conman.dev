c12e8bef9aa883722c779470e21244e0213642a7
Conmann <dcdraper14@hotmail.com>
2022-11-06 00:17:46 -0500

    deleting old commented code

--- index.js
+++ index.js
@@ -2,15 +2,3 @@ var customSettings = {},
 	scene;
 
 document.querySelectorAll('.S')[0].style.backgroundImage = ('url(assets/backgrounds/' + (Math.floor(Math.random() * 6) + 1) + '.jpg)');
-
-// var i = 0,
-// iaa = setInterval(function(){
-// 	document.querySelector(':root').style.setProperty('--viewW', i + 'vw');
-// 	document.querySelector(':root').style.setProperty('--viewH', 1.0471286**i + 'vh');
-// 	i++
-// 	if(i > 100){
-// 		clearInterval(iaa);
-// 	}
-// }, 4)
-
-// openSettings()
