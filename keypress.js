479eef6180257b8c662d991557940fcc69b1cd84
Conmann <dcdraper14@hotmail.com>
2022-12-17 23:20:30 -0600

    changed all CTRL+_ shortcuts to ALT+_
    also added header open/close functionality*
    
    *it looks kinda broke but it works

--- keypress.js
+++ keypress.js
@@ -1,10 +1,10 @@
-window.addEventListener('keydown', function(a){
+window.addEventListener('keydown', function(e){
 //* shortcuts
-if(a.key == 'g'){
-	if(true){
+if(e.key == 't' && e.altKey == true){
+	if(document.querySelector('.header').style.height == '5vh'){
 		closeHeader();
 	}else{
-
+		openHeader();
 	}
 }
 
